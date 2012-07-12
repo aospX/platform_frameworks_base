CyanogenMod/android_frameworks_base

Legacy touchscreen: Fix B key issue
Patchset 2: Clean code

Change-Id: Ib1fc59833d066dee9c4c5f02a9bf1a912463cb1e

services/input/InputReader.cpp
@@ -967,7 +967,7 @@ void InputDevice::process(const RawEvent* rawEvents, size_t count) {
 
             // Old touchscreen sensors need to send a fake BTN_TOUCH (BTN_LEFT)
 
-            if (rawEvent->scanCode == ABS_MT_TOUCH_MAJOR) {
+            if (rawEvent->scanCode == ABS_MT_TOUCH_MAJOR && rawEvent->type == EV_ABS) {
 
                 z_data = rawEvent->value;
                 touched = (0 != z_data);

aospX/platform_frameworks_base

services/input/InputReader.cpp
@@ -1004,7 +1004,7 @@ void InputDevice::process(const RawEvent* rawEvents, size_t count) {
 
             // Old touchscreen sensors need to send a fake BTN_TOUCH (BTN_LEFT)
 
-            if (rawEvent->code == ABS_MT_TOUCH_MAJOR) {
+            if (rawEvent->code == ABS_MT_TOUCH_MAJOR && rawEvent->type == EV_ABS) {
 
                 z_data = rawEvent->value;
                 touched = (0 != z_data);